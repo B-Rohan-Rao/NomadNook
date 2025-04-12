404887973ac82bdb6fe4bd9ad9007551de01cf5c
B-Rohan-Rao <raorohan060@gmail.com>
2025-04-13 02:01:54 +0530

    Providing more interface for users

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, 'views'));
@@ -21,10 +23,19 @@ async function main(){
     await mongoose.connect(MONGODB_URL);
 }
 
+/*
+app.get("/listings", ...)
+app.get("/listings/new", ...)
+app.post("/listings", ...)
+app.get("/listings/:id/edit", ...)
+app.put("/listings/:id", ...)
+app.delete("/listings/:id", ...)
+app.get("/listings/:id", ...)
+app.get("/", ...)
+app.all("*", ...)
 
-app.get("/",(req,res)=>{
-    res.send("This is root path");
-});
+*/
+
 
 // app.get("/testlisting",async (req,res)=>{
 //     let samplelisting= new Listing({
@@ -41,57 +52,76 @@ app.get("/",(req,res)=>{
 
 // Index route
 // GET /listing ->all listings
-app.get("/listings", async (req,res)=>{
+app.get("/listings", wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+}));
 //---------------------------------------------------------------------------------------------------------------------
 
-// New route
+// Create route
 // get /listings/new -> form -> post /listings ->Insert into db
 app.get("/listings/new", (req,res)=>{
     res.render("listings/new.ejs");
 });
-app.post("/listings", async(req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save()
-    res.redirect("/listings")
-});
+app.post("/listings", wrapAsync(
+    async(req,res,next)=>{
+        if(!req.body.listing){
+            throw new ExpressError(400,"Not Send valid data for listing")
+        }
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}));
 //---------------------------------------------------------------------------------------------------------------------
 
 // edit route
-// grt /listing/:id/edit ->edit form ->put /listing/:id/->submit
-app.get("/listings/:id/edit", async (req,res)=>{
+// get /listing/:id/edit ->edit form ->put /listing/:id/->submit
+app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-})
-app.put("/listings/:id", async(req, res)=>{
+}));
+app.put("/listings/:id", wrapAsync(async(req, res)=>{
     let { id } = req.params;
+    if(!req.body.listing){
+        throw new ExpressError(400,"Not Send valid data for listing")
+    }
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 //---------------------------------------------------------------------------------------------------------------------
 
 // Delete route
 
-app.delete("/listings/:id", async(req, res)=>{
+app.delete("/listings/:id", wrapAsync(async(req, res)=>{
     let{id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+}));
 
 //---------------------------------------------------------------------------------------------------------------------
 // Read route
-app.get("/listings/:id", async (req,res)=>{
+app.get("/listings/:id", wrapAsync(async (req,res)=>{
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+}));
+
+// Root path
+
+app.get("/",(req,res)=>{
+    res.send("This is root path");
 });
 
 
-
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message="Something went Wrong!"} = err;
+    res.status(statusCode).send(message);
+});
 app.listen(8080,(req,res)=>{
     console.log("App listening at port 8080");
 });
