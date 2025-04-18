a7c5b712a88be6fe8b0a962776eff5f1b5a944dd
B-Rohan-Rao <raorohan060@gmail.com>
2025-04-18 19:40:52 +0530

    started working on login methods

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
+const mongoose = require("mongoose");
+const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const userRouter = require("./routes/user.js")
 
 
 
@@ -26,7 +30,12 @@ const MONGODB_URL = "mongodb://127.0.0.1:27017/nomadnook"
 main().then(()=>console.log('MongoDB Connected')).catch((err)=>console.log(err));
 async function main(){
     await mongoose.connect(MONGODB_URL);
-};
+}
+
+app.get("/",(req,res)=>{
+    res.send("This is root path");
+});
+
 
 const sessionOptions = {
     secret: "mysupersecretcode",
@@ -40,20 +49,34 @@ const sessionOptions = {
 };
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.use(new LocalStrategy(sessionOptions));
+passport.use(User.createStrategy()); // 👈 This is the correct fix
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
    res.locals.success = req.flash("success");
    res.locals.error = req.flash("error");
    next(); 
 });
 
+// app.get("/demoUser", async (req,res)=>{
+//     let fakeUser = new User({
+//         email:"fakeuser@gmail.com",
+//         username: "Student",
+//     });
+//     let registerUser = await User.register(fakeUser, "helloworld");
+//     res.send(registerUser);
+// });
 
-
-app.get("/",(req,res)=>{
-    res.send("This is root path");
-});
-
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter)
 
 
 app.use((req, res, next) => {
